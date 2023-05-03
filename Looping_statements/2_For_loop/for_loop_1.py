97f044569d3720444b2896b4b682569173312a73
Lucky <luckyvishwa1104@gmail.com>
2023-05-04 00:04:56 +0530

    Enhanced the formate of code

--- Looping_statements/2_For_loop/for_loop_1.py
+++ Looping_statements/2_For_loop/for_loop_1.py
@@ -1,8 +1,7 @@
 #2. For loop - repeat the loop for each value of sequence.
 ''' Syntax:
 	
-	for (variable) in (range): # only this    
-	                                         format is allowed
+	for (variable) in (range): # only this format is allowed
 	                                         	             	
 		statement_1
 		statement_2
@@ -10,16 +9,20 @@
 		statement_n
 		- -
 	rest of code '''
+
 # example 1
 # program to print "hello" five times
 for i in range(1,6):
 		print("hello")
 print("end")
+
 # example 4
+2
 # program to print first 10 numbers
 for i in range(1,11):
 	print(i)
 print("end")
+
 # example 3
 # program to square of first 5 no.
 for num in range(1,6):
